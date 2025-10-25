7913ba1e1ec5acc0aeb5b808860360ede3b85849
FacundoLescano <facundoleonellescano@gmail.com>
2025-10-25 02:14:18 -0300

    load setting

--- modifit_platform/modifit_platform/settings.py
+++ modifit_platform/modifit_platform/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-p!o1*-0y2rke%rdgm*i)yo@kc5&32=@x-y#veh1h79sapx84d+"
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
